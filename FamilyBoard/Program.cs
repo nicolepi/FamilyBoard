28a6142e47c8ff1117a48fd792f3d081a6441089
Nicole Pi <nicole.pi424@gmail.com>
2016-11-03 23:28:00 -0700

    add photo function

--- FamilyBoard/Program.cs
+++ FamilyBoard/Program.cs
@@ -10,28 +10,59 @@ namespace FamilyBoard
     {
         static void Main(string[] args)
         {
-            Photo Picture1 = new Photo();
-            Picture1.Title = "Seattle Summer";
-            Board.addPhoto(Picture1);
+            Console.WriteLine("Welcome!");
+            int choice = -1; 
+            bool invalidChoice = false;
+            while (choice != 0 || invalidChoice)
+            {
+                Console.WriteLine("1. Upload a picture.");
+                Console.WriteLine("2. Upload a video.");
+                Console.WriteLine("3. Leave a comment.");
+                Console.WriteLine("0. Exit.");
+                Console.Write("Please choose an option: ");
+                var input = Console.ReadLine();
+                
+                if (!int.TryParse(input, out choice))
+                {
+                    invalidChoice = true;
+                    Console.WriteLine("Invalid choice, try again...");
+                    continue;
+                }
+                invalidChoice = false; 
+                switch (choice)
+                {
+                    case 1:
+                        {
+                            Console.Write("Enter Photo Title: ");
+                            var title = Console.ReadLine();
+                            var photo = new Photo();
+                            photo.Title = title;
+                            photo.DateCreated = DateTime.Now.ToString();
+                            Board.addPhoto(photo);
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Write("Enter Video Title: ");
+                            var title = Console.ReadLine();
+                            var video = new Video();
+                            video.Title = title;
+                            video.DateCreated = DateTime.Now.ToString();
+                            Board.addVideo(video);
+                            break;
+                        }
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-            Picture1 = new Photo();
-            Picture1.Title = "Winter Vacation";
-            Board.addPhoto(Picture1);
 
-            Video Video1 = new Video();
-            Video1.Title = "Poodle Mishmish's Birthday Party";
-            Board.addVideo(Video1);
 
-            User Nicole = new User();
-            Nicole.UserName = "nicolepi";
-            Nicole.FirstName = "Nicole";
-            Nicole.LastName = "Pi";
-            Board.addUser(Nicole);
-            
-                     
-            
             Board.PrintContent();
             Console.ReadLine(); //prevent command line from closing
+            Board.PrintContent();
         }
     }
 }
