9dbb8bf024084ad5dd169377324cd0e5307a0ddc
Nicole Pi <nicole.pi424@gmail.com>
2016-12-10 17:10:06 -0800

    add video player

---
+++ FamilyBoardUI/Controllers/VideoFileController.cs
@@ -0,0 +1,18 @@
+﻿using FamilyBoard;
+using FamilyBoardUI;
+using System.Web.Mvc;
+
+namespace FamilyBoardUI.Controllers
+{
+    public class VideoFileController : Controller
+    {
+        private FamilyBoardModel db = new FamilyBoardModel();
+        //
+        // GET: /File/
+        public ActionResult Index(int id)
+        {
+            var fileToRetrieve = db.VideoFiles.Find(id);
+            return File(fileToRetrieve.Content, fileToRetrieve.ContentType);
+        }
+    }
+}
