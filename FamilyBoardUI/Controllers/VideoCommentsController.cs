b40a75b7eb314c0f3778f24e21fc5e360d34fd4a
Nicole Pi <nicole.pi424@gmail.com>
2016-12-11 09:41:04 -0800

    before test

--- FamilyBoardUI/Controllers/VideoCommentsController.cs
+++ FamilyBoardUI/Controllers/VideoCommentsController.cs
@@ -8,29 +8,34 @@ using System.Web;
 using System.Web.Mvc;
 using FamilyBoard;
 
+//Controller
+
 namespace FamilyBoardUI.Controllers
 {
     public class VideoCommentsController : Controller
     {
         private FamilyBoardModel db = new FamilyBoardModel();
 
+        //original code
+        public ActionResult Index()
+        {
+            var videoComments = db.VideoComments.Include(p => p.Video).Include(p => p.User);
+            return View(videoComments.ToList());
+        }
+
+
         // GET: VideoComments
 
 
-
-        public ActionResult Index(int id)
+        public ActionResult ViewComment(int id)
         {
+
             var videoComments = from p in db.VideoComments where p.VideoId == id select p;
             return View(videoComments.ToList());
         }
 
 
 
-        //public ActionResult Index()
-        //{
-        //    var videoComments = db.VideoComments.Include(v => v.User).Include(v => v.Video);
-        //    return View(videoComments.ToList());
-        //}
 
         // GET: VideoComments/Details/5
         public ActionResult Details(int? id)
@@ -50,8 +55,9 @@ namespace FamilyBoardUI.Controllers
         // GET: VideoComments/Create
         public ActionResult Create()
         {
-            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName");
+
             ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName");
             return View();
         }
 
@@ -62,16 +68,17 @@ namespace FamilyBoardUI.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Content,DateCreated,VideoId,UserId")] VideoComment videoComment)
         {
+
             if (ModelState.IsValid)
             {
                 db.VideoComments.Add(videoComment);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index", "Videos");
             }
 
-            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
             ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title", videoComment.VideoId);
-            return View(videoComment);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
+            return RedirectToAction("Index", "Videos");
         }
 
         // GET: VideoComments/Edit/5
@@ -86,8 +93,8 @@ namespace FamilyBoardUI.Controllers
             {
                 return HttpNotFound();
             }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
             ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title", videoComment.VideoId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
             return View(videoComment);
         }
 
@@ -104,8 +111,8 @@ namespace FamilyBoardUI.Controllers
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
             ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title", videoComment.VideoId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
             return View(videoComment);
         }
 
@@ -145,3 +152,146 @@ namespace FamilyBoardUI.Controllers
         }
     }
 }
+
+//using System;
+//using System.Collections.Generic;
+//using System.Data;
+//using System.Data.Entity;
+//using System.Linq;
+//using System.Net;
+//using System.Web;
+//using System.Web.Mvc;
+//using FamilyBoard;
+
+//namespace FamilyBoardUI.Controllers
+//{
+//    public class VideoCommentsController : Controller
+//    {
+//        private FamilyBoardModel db = new FamilyBoardModel();
+
+//        public ActionResult Index()
+//        {
+//            var videoComments = db.VideoComments.Include(p => p.Video).Include(p => p.User);
+//            return View(videoComments.ToList());
+//        }
+
+
+//        public ActionResult ViewComment(int id)
+//        {
+
+//            var videoComments = from p in db.VideoComments where p.VideoId == id select p;
+//            return View(videoComments.ToList());
+//        }
+
+//        public ActionResult Details(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            VideoComment videoComment = db.VideoComments.Find(id);
+//            if (videoComment == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(videoComment);
+//        }
+
+//        // GET: VideoComments/Create
+//        public ActionResult Create()
+//        {
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName");
+//            ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title");
+//            return View();
+//        }
+
+//        // POST: VideoComments/Create
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Create([Bind(Include = "Id,Content,DateCreated,VideoId,UserId")] VideoComment videoComment)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.VideoComments.Add(videoComment);
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
+//            ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title", videoComment.VideoId);
+//            return View(videoComment);
+//        }
+
+//        // GET: VideoComments/Edit/5
+//        public ActionResult Edit(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            VideoComment videoComment = db.VideoComments.Find(id);
+//            if (videoComment == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
+//            ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title", videoComment.VideoId);
+//            return View(videoComment);
+//        }
+
+//        // POST: VideoComments/Edit/5
+//        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+//        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+//        [HttpPost]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult Edit([Bind(Include = "Id,Content,DateCreated,VideoId,UserId")] VideoComment videoComment)
+//        {
+//            if (ModelState.IsValid)
+//            {
+//                db.Entry(videoComment).State = EntityState.Modified;
+//                db.SaveChanges();
+//                return RedirectToAction("Index");
+//            }
+//            ViewBag.UserId = new SelectList(db.Users, "Id", "UserName", videoComment.UserId);
+//            ViewBag.VideoId = new SelectList(db.Videos, "Id", "Title", videoComment.VideoId);
+//            return View(videoComment);
+//        }
+
+//        // GET: VideoComments/Delete/5
+//        public ActionResult Delete(int? id)
+//        {
+//            if (id == null)
+//            {
+//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//            }
+//            VideoComment videoComment = db.VideoComments.Find(id);
+//            if (videoComment == null)
+//            {
+//                return HttpNotFound();
+//            }
+//            return View(videoComment);
+//        }
+
+//        // POST: VideoComments/Delete/5
+//        [HttpPost, ActionName("Delete")]
+//        [ValidateAntiForgeryToken]
+//        public ActionResult DeleteConfirmed(int id)
+//        {
+//            VideoComment videoComment = db.VideoComments.Find(id);
+//            db.VideoComments.Remove(videoComment);
+//            db.SaveChanges();
+//            return RedirectToAction("Index");
+//        }
+
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing)
+//            {
+//                db.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
+//    }
+//}
